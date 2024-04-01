b672a15512d915073460305b34f09558801bc4c6
marcin p. joachimiak <4625870+realmarcin@users.noreply.github.com>
2024-03-31 23:18:17 -0700

    final PwC results

--- annotate/AIO_only.sh
+++ annotate/AIO_only.sh
@@ -1,1 +1,7 @@
 grep -B 1 -A 5 'aio/' methods_reformat_annotate.out   > methods_reformat_annotate_AIOonly.txt
+
+grep -B 1 -A 5 'aio/' methods_reformat_collection_annotate.out > methods_reformat_collection_annotate_AIOonly.txt
+
+grep -B 1 -A 5 'aio/' methods_reformat_area_annotate.out >methods_reformat_area_annotate_AIOonly.txt
+
+
--- annotate/run.sh
+++ annotate/run.sh
@@ -1,4 +1,13 @@
-runoak -i ../aio.owl annotate --text-file methods_reformat.txt &> methods_reformat_annotate.out
+
+grep 'area\":' methods_reformat.txt | sort | uniq > methods_reformat_area.txt
+grep 'collection\":' methods_reformat.txt | sort | uniq > methods_reformat_collection.txt
+
+runoak -i ../aio.owl annotate --text-file methods_reformat.txt > methods_reformat_annotate.out
+
+# --matches-whole-text
+runoak -i ../aio.owl annotate --text-file methods_reformat_area.txt > methods_reformat_area_annotate.out
+runoak -i ../aio.owl annotate --text-file methods_reformat_collection.txt > methods_reformat_collection_annotate.out
+
 source AIO_only.sh
 source PWC_AIO_general_counts.sh
 source PWC_AIO_counts.sh
