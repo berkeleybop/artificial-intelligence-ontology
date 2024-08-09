9472780d910cd8f5a8ad67ea8f8452694f87f533
marcin p. joachimiak <4625870+realmarcin@users.noreply.github.com>
2024-08-09 13:14:41 -0700

    lexmatch update

--- src/mappings/run.sh
+++ src/mappings/run.sh
@@ -1,8 +1,6 @@
 runoak -i sqlite:obo:cso -a sqlite:obo:aio lexmatch i^cso @ i^aio -o aio-to-cso.sssom_full.tsv
 runoak -i sqlite:obo:ncit -a sqlite:obo:aio lexmatch i^ncit @ i^aio -o aio-to-ncit.sssom_full.tsv
-runoak -i sqlite:obo:ito -a sqlite:obo:aio lexmatch i^ito @ i^aio -o aio-to-ito.sssom_full.tsv
 runoak -i sqlite:obo:edam -a sqlite:obo:aio lexmatch i^edam @ i^aio -o aio-to-edam.sssom_full.tsv
-runoak -i sqlite:obo:obiws -a sqlite:obo:aio lexmatch i^obiws @ i^aio -o aio-to-obiws.sssom_full.tsv
 runoak -i sqlite:obo:mlo -a sqlite:obo:aio lexmatch i^mlo @ i^aio -o aio-to-mlo.sssom_full.tsv
 runoak -i sqlite:obo:swo -a sqlite:obo:aio lexmatch i^swo @ i^aio -o aio-to-swo.sssom_full.tsv
 
